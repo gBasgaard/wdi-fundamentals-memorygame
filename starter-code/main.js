cfa7e919c065427e40467ad042244fbf57295098
gBasgaard <glenn_basgaard@yahoo.com>
2016-10-11 20:40:15 -0400

    update cards with the DOM

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,10 +5,22 @@ var cardTwo = 'queen';
 var cardThree = 'king';
 var cardFour = 'king';
 
-if (cardTwo === cardFour) {
-	alert("Sorry, try again.");
-} else if (cardOne === cardTwo) {
+// if (cardTwo === cardFour) {
+// 	alert("Sorry, try again.");
+// } else if (cardOne === cardTwo || cardThree === cardFour) {
+// 	alert("You found a match!");
+// } else if (cardThree === cardFour) {
 
-} else if (cardThree === cardFour) {
+// }
 
+var board = document.getElementById('game-board');
+
+var createCards = function() {
+	for (var i = 0; i < 4; i++) {
+		var cardElement = document.createElement('div');
+		cardElement.className = 'card';
+		board.appendChild(cardElement);
+	}
 }
+
+createCards();
